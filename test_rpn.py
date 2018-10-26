287269d4b35e5c15f7084629748ed5b8d903966f
sbein <sbein@umich.edu>
2018-10-26 11:25:45 -0400

    fixed syntax error lol

--- test_rpn.py
+++ test_rpn.py
@@ -6,6 +6,6 @@ class TestBasics(unittest.TestCase):
         result = rpn.calculate('1 1 +')
         self.assertEqual(2,result)
 
-    def test_subtract(self)
+    def test_subtract(self):
         result = rpn.calculate('4 3 -')
         self.assertEqual(1, result)
