27b00a663d36595c521c5720d9c853c6ef988a34
sbein <sbein@umich.edu>
2018-10-26 11:29:38 -0400

    add tests for * and /

--- test_rpn.py
+++ test_rpn.py
@@ -9,3 +9,11 @@ class TestBasics(unittest.TestCase):
     def test_subtract(self):
         result = rpn.calculate('4 3 -')
         self.assertEqual(1, result)
+
+    def test_multiply(self):
+        result = rpn.calculate('3 4 *')
+        self.assertEqual(12, result)
+
+    def test_divide(self):
+        result = rpn.calculate('12 6 /')
+        self.assertEqual(2, result)
