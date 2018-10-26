ed23a199b5bb6886dfe0b438d65645a26291465a
sbein <sbein@umich.edu>
2018-10-26 11:18:16 -0400

    Passed first + test.

--- rpn.py
+++ rpn.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 
 def calculate(arg):
-    pass
+    # stack for calculator
+    stack = []
 
-def main():
+    # tokenize input
+    tokens = arg.split()
+
+    # process tokens
+    for token in tokens:
+        try:
+                value = int(token)
+                stack.append(value)
+        except ValueError:
+                val1 = stack.pop()
+                val2 = stack.pop()
+                result = val1 + val2
+                stack.append(result)
+                return stack[0]
+        
+
+def main():     
     while True:
         calculate(input('rpn calc> '))
 
